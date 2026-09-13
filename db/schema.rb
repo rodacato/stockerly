dce74844c7a32ee0dc53b6c16c14d3a160147b68
Rogelio Adrian Castillo Toscano <rodacato@gmail.com>
2026-09-13 22:31:04 +0000

    Drop users.consents_data_processing_at
    Added for the closed beta's consent flow and never read or written by
    any code since. With the privacy notice gone there is no consent for it
    to record.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_12_180000) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_13_223038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -536,7 +536,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_12_180000) do
 
   create_table "users", force: :cascade do |t|
     t.string "avatar_url"
-    t.datetime "consents_data_processing_at"
     t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "full_name", null: false
