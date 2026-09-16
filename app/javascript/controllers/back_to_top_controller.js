50d07c70bbc35d34b55f6e4466769a135b0bdd62
Rogelio Adrian Castillo Toscano <rodacato@gmail.com>
2026-09-16 14:43:02 +0000

    Delete the back-to-top button nothing renders

--- app/javascript/controllers/back_to_top_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class BackToTopController extends Controller {
-  scroll(event) {
-    event.preventDefault()
-    window.scrollTo({ top: 0, behavior: "smooth" })
-  }
-}
